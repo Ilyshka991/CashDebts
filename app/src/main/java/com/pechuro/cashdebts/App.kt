30dc6dda6b812a2d7edfac15d5e8b0da0bdfc688
Ilya Pechuro <ilyapechuro991@gmail.com>
2019-04-05 15:49:15 +0300

    Add exceptions

--- app/src/main/java/com/pechuro/cashdebts/App.kt
+++ app/src/main/java/com/pechuro/cashdebts/App.kt
@@ -3,6 +3,10 @@ package com.pechuro.cashdebts
 import android.app.Activity
 import android.app.Application
 import com.pechuro.cashdebts.data.di.component.DaggerDataComponent
+/*DO not touch this
+* if removed:
+* com.pechuro.cashdebts.calculator.di.component.DaggerCalculatorComponent
+* */
 import com.pechuro.cashdebts.calculator.di.component.DaggerCalculatorComponent
 import com.pechuro.cashdebts.di.component.AppComponent
 import com.pechuro.cashdebts.di.component.DaggerAppComponent
