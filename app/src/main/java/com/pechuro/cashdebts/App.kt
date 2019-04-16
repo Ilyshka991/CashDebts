10c50cccf96968e280f783cedaecb2cb35c412c4
Ilya Pechuro <ilyapechuro991@gmail.com>
2019-04-16 11:06:06 +0300

    Remove leak canary

--- app/src/main/java/com/pechuro/cashdebts/App.kt
+++ app/src/main/java/com/pechuro/cashdebts/App.kt
@@ -23,8 +23,8 @@ class App : Application(), HasActivityInjector {
     override fun onCreate() {
         super.onCreate()
 
-        if (LeakCanary.isInAnalyzerProcess(this)) return
-        LeakCanary.install(this)
+     /*   if (LeakCanary.isInAnalyzerProcess(this)) return
+        LeakCanary.install(this)*/
 
         initDI()
     }
