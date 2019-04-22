4e4a8fa3e7a5215c6a42a20d26c0aa9f5b8d27e4
Ilya Pechuro <ilyapechuro991@gmail.com>
2019-04-23 01:00:31 +0300

    Add FCM

---
+++ app/src/main/java/com/pechuro/cashdebts/service/FCMService.kt
@@ -0,0 +1,11 @@
+package com.pechuro.cashdebts.service
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class FCMService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(msg: RemoteMessage?) {
+
+    }
+}
